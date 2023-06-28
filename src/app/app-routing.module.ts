1c47b8a9cbf82fafc8b3f5310e2789b943c9fa88
andrew_smith_moser <andrew.smith@moserit.com>
2023-06-28 10:21:18 -0400

    tailwind set up and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { SignUpCardComponent } from './components/sign-up-card/sign-up-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SignUpCardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/sign-up-card/sign-up-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sign-up-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sign-up-card.component.html',
+  styleUrls: ['./sign-up-card.component.scss']
+})
+export class SignUpCardComponent {
+
+}
