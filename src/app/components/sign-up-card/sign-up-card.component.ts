a6e660465492b93416ed4f8c9f8d226d1626b995
andrew_smith_moser <andrew.smith@moserit.com>
2023-06-29 12:25:07 -0400

    Added configs and initail styling/content

--- src/app/components/sign-up-card/sign-up-card.component.ts
+++ src/app/components/sign-up-card/sign-up-card.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up-card.component.html',
-  styleUrls: ['./sign-up-card.component.scss']
+  styleUrls: ['./sign-up-card.component.scss'],
 })
 export class SignUpCardComponent {
+  submissionForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.submissionForm = this.formBuilder.group({
+      emailAddress: ['', [Validators.required]],
+    });
+  }
+
+  showError(control: string): boolean {
+    const hasError =
+      this.submissionForm.controls['emailAddress'].errors &&
+      (this.submissionForm.controls['emailAddress'].touched ||
+        this.submissionForm.controls['emailAddress'].dirty);
+    return hasError ?? false;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      listStyleImage: {
+        checkmark: "url('./assets/images/icon-list.svg')",
+      },
+      colors: {
+        tomato: 'hsl(4, 100%, 67%)',
+        'dark-slate-grey': 'hsl(234, 29%, 20%)',
+        'charcoal-grey': 'hsl(235, 18%, 26%)',
+        grey: 'hsl(231, 7%, 60%)',
+        pink: 'hsl(346, 100%, 66%)',
+        orange: 'hsl(13, 90%, 60%)',
+      },
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 };
